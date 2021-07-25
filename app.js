ac45f6811229e532607f950e88819db08656be0e
Pupass Chandanamattha <74857873+poohcom1@users.noreply.github.com>
2021-07-25 22:14:20 +0700

    Restructured routes

--- app.js
+++ app.js
@@ -1,15 +1,12 @@
 const express = require('express');
 const path = require('path');
-const mongoose = require('mongoose');
 const session = require('express-session');
 const MongoStore = require('connect-mongo');
 const passport = require("passport");
 require('ejs')
 
-const routes = require('./routes')
-
-
-if (process.env.NODE_ENV === "development") require('dotenv').config(); // Only use .env in development mode
+// Only use .env in development mode
+if (process.env.NODE_ENV === "development") require('dotenv').config();
 
 // Init app
 const app = express();
@@ -26,27 +23,16 @@ app.use(express.static(path.join(__dirname, 'public')))
 app.use('/css', express.static(path.join(__dirname, 'public/css')))
 app.use('/js', express.static(path.join(__dirname, 'public/js')))
 
-/* --------------------------------- Database ------------------------------- */
-const dbUri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}/${process.env.DB_NAME}?retryWrites=true&w=majority`;
-const dbOptions = { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true }
-
-// Connect to database
-const clientPromise = mongoose.connect(dbUri, dbOptions)
-    .then(m => {
-        console.log(`[mongodb] Connected to database <${process.env.DB_NAME}>`);
-        app.emit("ready");
-
-        return m.connection.getClient(); // Return the client to use with connect-mongo
-    })
-    .catch(err => console.log('[Database error] ' + err));
-
 // Create session with connect mongo
+const { mongoosePromise, mongoOptions } = require('./config/database.js')
+
 app.use(session({
     secret: process.env.SESSION_SECRET,
     resave: false,
     saveUninitialized: true,
     store: MongoStore.create({
-        clientPromise: clientPromise,
+        clientPromise: mongoosePromise.then(m => m.connection.getClient()),
+        mongoOptions: mongoOptions,
         dbName: process.env.DB_NAME,
         collectionName: 'sessions',
         autoRemove: 'interval',
@@ -61,11 +47,20 @@ require('./config/passport.js')
 app.use(passport.initialize())
 app.use(passport.session())
 
+// Routes setup
+require('./routes')(app)
 
-// Routes
-app.use(routes)
+// Error handling
+app.use((err, req, res, next) => {
+    res.status(500).send('Something broke!')
+})
 
-app.on("ready", () => {
+mongoosePromise.then(() => {
     /* App started here */
-    app.listen(process.env.PORT, () => console.log(`[app] Server running on port ${process.env.PORT} in <${process.env.NODE_ENV}> mode`));
+    const server = app.listen(process.env.PORT, () => {
+
+        const address = process.env.NODE_ENV === 'development' ? `http://localhost:${ process.env.PORT }` : server.address().address;
+
+        console.log(`[app] Server running on ${ address } in <${ process.env.NODE_ENV }> mode`)
+    });
 })
